aff650f1e7f84669672bcca4418a0c79c23714a4
Simon Martin <smartin@scottlogic.com>
2024-12-18 09:44:38 +0000

    Day 2 part 1 complete

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -5,8 +5,14 @@ use tracing::{debug, event_enabled, info, Level};
 pub type ResultType = u64;
 
 #[derive(Debug, Default)]
-pub struct Solution {}
-impl Solution {}
+pub struct Solution {
+    rows: Vec<Vec<ResultType>>,
+}
+impl Solution {
+    fn add_row(&mut self, row: Vec<ResultType>) {
+        self.rows.push(row);
+    }
+}
 
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
@@ -16,6 +22,8 @@ impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
         let mut solution = Self::default();
         for (id, line) in reader.lines().map_while(Result::ok).enumerate() {
             // Implement for problem
+            let row = line.split_whitespace().map(|v| v.parse().unwrap()).collect();
+            solution.add_row(row);
         }
         Ok(solution)
     }
@@ -25,8 +33,14 @@ impl utils::Solution for Solution {
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        let mut r = 0;
+        for row in &self.rows {
+            let max = row.iter().max().unwrap();
+            let min = row.iter().min().unwrap();
+            r += max - min;
+        }
         // Implement for problem
-        Ok(0)
+        Ok(r)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
